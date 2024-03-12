dfd2bc7c0d6f12eaebef3f40883a4e38d395fecb
ds-muzalevskiy <74115150+ds-muzalevskiy@users.noreply.github.com>
2024-03-12 16:05:59 +0100

    Actual Slackbot Manu Version

--- slackbot_chatgpt.py
+++ slackbot_chatgpt.py
@@ -1,6 +1,10 @@
 SLACK_BOT_TOKEN = ""
 SLACK_APP_TOKEN = ""
-OPENAI_API_KEY  = ""
+
+API_TYPE = ""
+API_BASE = ""
+API_VERSION = ""
+API_KEY = ""
 
 import os
 import openai
@@ -20,20 +24,30 @@ def handle_message_events(body, logger):
 
     response = client.chat_postMessage(channel=body["event"]["channel"],
                                        thread_ts=body["event"]["event_ts"],
-                                       text=f"Hello from your bot! :robot_face: \nThanks for your request!")
+                                       text=f"Hello from your ScoutBot! :robot_face: \nThanks for your request, I will try to answer it as soon as possible!")
 
-    openai.api_key = OPENAI_API_KEY
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5).choices[0].text
+    openai.api_type = API_TYPE
+    openai.api_base = API_BASE
+    openai.api_version = API_VERSION
+    openai.api_key = API_KEY
+
+    response = openai.ChatCompletion.create(
+        engine="demo-ai",
+        messages = [
+            {"role":"user", "content":prompt}
+            ],
+        temperature=0.5,
+        max_tokens=500,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+        ).choices[0]["message"]["content"]
 
     response = client.chat_postMessage(channel=body["event"]["channel"],
                                        thread_ts=body["event"]["event_ts"],
                                        text=f"Here you go: \n{response}")
+
 
 if __name__ == "__main__":
     SocketModeHandler(app, SLACK_APP_TOKEN).start()
